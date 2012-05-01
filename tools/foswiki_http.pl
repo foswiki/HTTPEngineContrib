7c1c194cac6f9f89e4c3a1476c2721b6b0ff4d23
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-01 14:07:26 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule
    git-svn-id: http://svn.foswiki.org/trunk/HTTPEngineContrib@14680 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- tools/foswiki_http.pl
+++ tools/foswiki_http.pl
@@ -18,8 +18,8 @@ use strict;
 use Foswiki::Engine::HTTP::Server;
 
 my $http = Foswiki::Engine::HTTP::Server->new(
-    server_type        => 'Single',
-    no_client_stdout   => 1,
-    log_level          => 0,
+    server_type      => 'Single',
+    no_client_stdout => 1,
+    log_level        => 0,
 );
 $http->run( host => '127.0.0.1', port => 8080, proto => 'tcp' );
